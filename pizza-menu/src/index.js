79106b2f1e9c203539236595c49d5073aea5ebe8
balrumeys <balrumeyst@gmail.com>
2026-01-31 03:11:38 +0300

    (feat) pizza-menu: conditionally render UI with ternary operator

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -71,19 +71,21 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData;
-  //const pizzas = [];
+  // const pizzas = []; //The advantage of using the ternary operator is that if the data is empty, we can return null or the value we want.
   const numPizzas = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu.Please come back later.</p>
       )}
 
       {/*
@@ -129,11 +131,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00.Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
